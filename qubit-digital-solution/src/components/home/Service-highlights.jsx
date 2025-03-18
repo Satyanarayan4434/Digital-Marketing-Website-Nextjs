26d3ce560a80cf12dd5bdfa524000a813f261f05
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2025-03-18 22:05:12 +0530

    Update Service-highlights.jsx

--- qubit-digital-solution/src/components/home/Service-highlights.jsx
+++ qubit-digital-solution/src/components/home/Service-highlights.jsx
@@ -2,18 +2,12 @@ import Link from "next/link";
 import { IoIosArrowForward } from "react-icons/io";
 import Slider from "@/components/Slider";
 import SliderDesktop from "@/components/SliderDesktop";
-import Slider from "@/components/ui/Slider";
 
 export default function ServiceHighlights() {
   return (
     <div className=" bg-[#d8d9df]">
       <div className="flex flex-col items-start justify-start pt-[2rem] gap-2 lg:ml-[5rem]">
         <div className="text-xl lg:text-4xl font-bold lg:w-[50rem] w-[20rem]">
-          Empowering brands with websites, SEO, social media, content, and
-          advertising solutions.
-    <div>
-      <div>
-        <div>
           Empowering brands with websites, SEO, social media, content, and
           advertising solutions.
         </div>
@@ -32,19 +26,6 @@ export default function ServiceHighlights() {
       <div className="hidden lg:flex">
         <SliderDesktop />
       </div>
-        <div>
-          We craft stunning websites and deliver powerful digital marketing
-          strategies to boost visibility, drive engagement, and maximize
-          business growth.
-        </div>
-        <div>
-          Contact us now <IoIosArrowForward />
-        </div>
-      </div>
-      <div>
-        <Slider />
-      </div>
-      <div></div>
     </div>
   );
 }
